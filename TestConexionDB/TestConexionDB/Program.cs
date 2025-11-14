da2973851424c1a6d617f6f65325ede42d456ce3
MatiasFernandez13 <112591273+MatiasFernandez13@users.noreply.github.com>
2025-11-14 19:45:26 -0300

    Add files via upload

---
+++ TestConexionDB/TestConexionDB/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+
+class Program
+{
+    static void Main()
+    {
+        string connectionString = "Data Source=localhost;Initial Catalog=BaseGestionBebidasMF;Integrated Security=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Conexión exitosa a la base de datos.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+        Console.ReadLine();
+    }
+}
