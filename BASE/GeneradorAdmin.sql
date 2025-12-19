80f862b760f792276d571727bad3894f19bc5d18
MatiasFernandez13 <112591273+MatiasFernandez13@users.noreply.github.com>
2025-12-18 21:00:34 -0300

    Delete BASE directory

--- BASE/GeneradorAdmin.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Usuarios (NombreUsuario, Contraseña, Salt, RolId)
-VALUES ('admin', 
-        'PONER_HASH_DE_LA_CONTRASEÑA_AQUÍ', 
-        'PONER_SALT_AQUÍ', 
-        1);
--- BASE/Roles y Cat.sql
+++
@@ -1,5 +0,0 @@
--- Insertar roles
-INSERT INTO Roles (Nombre) VALUES ('Administrador'), ('Vendedor');
-
--- Insertar categorías
-INSERT INTO Categorias (Nombre) VALUES ('Alcohólica'), ('No Alcohólica');
